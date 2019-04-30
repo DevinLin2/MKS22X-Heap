b5d586c1550b9b14c414f8e813bc9f823c9f972d
Devin Lin <dlin01@stuy.edu>
2019-04-30 08:10:55 -0400

    skeleton of functions

---
+++ MyHeap.java
@@ -0,0 +1,16 @@
+public class MyHeap {
+  private static void pushDown(int[]data,int size,int index) {
+    if (size >= 0 && size <= data.length - 1 && index >= 0 && index <= size - 1) {
+      while ((index * 2 + 1 <= size - 1 || index * 2 + 2 <= size - 1) && )
+    }
+  }
+  private static void pushUp(int[]data,int index) {
+
+  }
+  public static void heapify(int[]) {
+
+  }
+  public static void heapsort(int[]) {
+
+  }
+}
