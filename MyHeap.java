4668b7d4cd2326bf56d022aefa08d03865c38e02
VitaminOh <dlin01@stuy.edu>
2019-04-30 10:16:57 -0400

    almost finished pushdown

--- MyHeap.java
+++ MyHeap.java
@@ -1,8 +1,17 @@
 public class MyHeap {
-  private static void pushDown(int[]data,int size,int index) {
+  private static void pushDown(int[]data, int size, int index) {
+    int max = -1;
+    int newIndex = 0;
     if (size >= 0 && size <= data.length - 1 && index >= 0 && index <= size - 1) {
-      while ((index * 2 + 1 <= size - 1 || index * 2 + 2 <= size - 1) && )
+      if (index * 2 + 1 <= size - 1) {
+        max = data[index * 2 + 1];
+        newIndex = index * 2 + 1;
+      }
+      if (index * 2 + 2 <= size - 1 && data[index * 2 + 1] > max) {
+        newIndex = index * 2 + 2;
+      }
     }
+    // recursive call here
   }
   private static void pushUp(int[]data,int index) {
 
