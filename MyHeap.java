ae7d73d302853a17369f75d665b8aeb58e3787ff
Devin Lin <dlin01@stuy.edu>
2019-05-01 13:56:50 -0400

    finished typing the code for pushDown but have not tested

--- MyHeap.java
+++ MyHeap.java
@@ -1,7 +1,19 @@
 public class MyHeap {
-  private static void pushDown(int[]data,int size,int index) {
+  private static void pushDown(int[] data, int size, int index) {
+    int newIndex = -1;
+    int oldData = data[index];
     if (size >= 0 && size <= data.length - 1 && index >= 0 && index <= size - 1) {
-      while ((index * 2 + 1 <= size - 1 || index * 2 + 2 <= size - 1) && )
+      if (index * 2 + 1 <= size - 1 && data[index * 2 + 1] > data[index]) { // if the node isnt a leaf
+        if (index * 2 + 2 <= size - 1 && data[index * 2 + 2] > data[index * 2 + 1] && data[index * 2 + 2] > data[index]) { // if the parent node has 2 children
+          newIndex = data[index * 2 + 2];
+        }
+        newIndex = data[index * 2 + 1];
+      }
+      if (newIndex != -1) {
+        data[index] = data[newIndex];
+        data[newIndex] = oldData;
+        pushDown(data, size, newIndex);
+      }
     }
   }
   private static void pushUp(int[]data,int index) {
